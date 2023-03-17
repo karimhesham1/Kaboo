2ac5e380fba5c96b321b69547cbe334cf262f7f0
Karim Hesham <karim.hesham5861@gmail.com>
2023-03-17 23:37:24 +0200

    gamed

--- Game.js
+++ Game.js
@@ -1,3 +1,148 @@
+class Card
+{
+	constructor(rank, suit)
+	{
+		this.rank = rank;
+		this.suit = suit;
+		this.value = this._calculateValue(rank, suit);
+	}
+	
+	
+	_calculateValue(rank, suit) 
+	{
+		if (rank === "A")
+		{
+			return 1;
+		} 
+		else if (rank == "K" && (suit == "diamonds"  || suit == "hearts"))
+		{
+			return -1;
+		}
+		else if (["K", "Q", "J"].includes(rank)) 
+		{
+			return 10;
+		} 
+		else 
+		{
+			return parseInt(rank);
+		}
+	}
+
+
+	compare(card) {
+		if (this.rank === card.rank) {
+		  return this.suit.localeCompare(card.suit);
+		} else {
+		  return this.rank.localeCompare(card.rank);
+		}
+	  }
+
+	getRank() 
+	{
+		return this.rank
+	}
+
+	getSuit()
+	{
+		return this.suit
+	}
+	
+	getValue()
+	{
+		return this.value
+	}
+
+	
+}
+
+
+
+class Deck {
+  constructor() {
+    this.cards = [];
+    this.createDeck();
+    this.shuffleDeck();
+  }
+
+  createDeck() {
+    const suits = ['hearts', 'diamonds', 'clubs', 'spades'];
+    const ranks = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
+    for (let suit in suits) {
+      for (let rank in ranks) {
+        this.cards.push(new Card(rank, suit));
+      }
+    }
+  }
+
+  shuffleDeck() {
+    let currentIndex = this.cards.length;
+    let randomIndex;
+    while (currentIndex !== 0) {
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+      [this.cards[currentIndex], this.cards[randomIndex]] = [this.cards[randomIndex], this.cards[currentIndex]];
+    }
+  }
+
+  drawCard() {
+    return this.cards.pop();
+  }
+}
+
+
+
+
+class Player {
+  constructor(name) {
+    this.name = name;
+    this.cards = [];
+    this.points = 0;
+  }
+
+  addCard(card) {
+    this.cards.push(card);
+  }
+
+  removeCard(index) {
+    return this.cards.splice(index, 1)[0];    //removes the card at the specified index and returns it
+  }
+
+  updatepoints()
+  {
+    let tmp = 0;
+    for(let i = 0 ; i<this.cards.length ; i++)
+    {
+      tmp = tmp + this.cards[i].getValue();
+    }
+    this.points = tmp;
+  }
+
+  
+
+  getCard(index) {
+    return this.cards[index];
+  }
+
+  getCards() {
+    return this.cards;
+  }
+
+  getNumCards() {
+    return this.cards.length;
+  }
+
+  hasCard(rank, suit) {
+    return this.cards.some(card => card.rank === rank && card.suit === suit);
+  }
+
+  hasSpecialCard() {
+    return this.cards.some(card => card.value === -1 || card.rank === 'J' || card.rank === 'Q' || card.rank === 'K');
+  }
+  
+}
+
+
+
 class Kaboo {
     constructor() {
       this.players = [new Player("alo 1"), new Player("alo 2"), new Player("alo 3"), new Player("alo 4")];
@@ -36,6 +181,7 @@ class Kaboo {
     resolveCardEffect(card) {
         
       // Implement card effect logic
+
     }
   
     kaboo() {
@@ -74,9 +220,18 @@ class Kaboo {
     playRound() {
       if (this.isGameOver()) return;
   
-      const card = this.deck.drawCard();
+      const currentcard = this.deck.drawCard();
       const currentPlayer = this.getCurrentPlayer();
 
+      const discardbutton = document.querySelector('#discard-card');
+      
+      discardbutton.addEventListener('click', () => {
+        this.discardPile = currentcard;
+        console.log('discard card button clicked');
+        console.log(this.discardPile);
+        this.nextPlayer();
+      });
+      
   
       // Implement game logic here, including swapping and discarding cards, and applying card effects
   
@@ -85,7 +240,7 @@ class Kaboo {
   }
   
   const game = new Kaboo();
-  while (!game.isGameOver()) {
+ // while (!game.isGameOver()) {
     game.playRound();
-  }
+ // }
   
